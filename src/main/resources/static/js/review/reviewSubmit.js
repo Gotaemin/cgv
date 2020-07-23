c2bf48da84146f33173ecaf67ff69a23acd85b4a
thdus3009 <thdus3009@gmail.com>
2020-07-23 15:54:20 +0900

    dd

--- src/main/resources/static/js/review/reviewSubmit.js
+++ src/main/resources/static/js/review/reviewSubmit.js
@@ -385,7 +385,7 @@
 		
 		/* ---------내 평점 보기---------- */
 		$("#look").click(function(){
-			window.location.href='http://${pageContext.request.contextPath}/review/reviewLook';
+			window.location.href='${pageContext.request.contextPath}/review/reviewLook';
 		});
 		
 /*		function reviewLook(curPage){
